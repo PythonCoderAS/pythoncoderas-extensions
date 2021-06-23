4aff38ae3e7ad3aa4e4991f0fcecd826da47d1a1
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2021-06-23 07:29:11 -0400

    Add cloudflare bypass

--- src/Hachirumi/Hachirumi.ts
+++ src/Hachirumi/Hachirumi.ts
@@ -1,11 +1,11 @@
-import {SourceInfo, TagType} from "paperback-extensions-common"
+import {Request, SourceInfo, TagType} from "paperback-extensions-common"
 import {GuyaTemplate} from "../GuyaTemplate";
 
 const BASE = "https://hachirumi.com"
 
 export const HachirumiInfo: SourceInfo = {
     icon: "icon.png",
-    version: "1.0.2",
+    version: "1.1.0",
     name: "Hachirumi",
     author: "PythonCoderAS",
     authorWebsite: "https://github.com/PythonCoderAS",
@@ -17,10 +17,21 @@ export const HachirumiInfo: SourceInfo = {
         {
             text: "Notifications",
             type: TagType.GREEN
+        },
+        {
+            text: "Cloudflare",
+            type: TagType.RED
         }
     ]
 }
 
 export class Hachirumi extends GuyaTemplate {
     readonly baseUrl: string = BASE;
+
+    getCloudflareBypassRequest(): Request {
+        return createRequestObject({
+            url: this.baseUrl,
+            method: "GET"
+        });
+    }
 }
